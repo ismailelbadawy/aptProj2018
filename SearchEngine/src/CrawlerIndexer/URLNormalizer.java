86fa6cdb7163d60a122b0d436a7073c2dd77642e
muhammad20 <muhammadahmad201196@hotmail.com>
2018-05-01 18:05:28 +0200

    page popularity rank calculated

---
+++ SearchEngine/src/CrawlerIndexer/URLNormalizer.java
@@ -0,0 +1,23 @@
+package CrawlerIndexer;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class URLNormalizer {
+    public static String normalize(String url) {
+        try {
+            URI uri = new URI(url);
+            url = uri.normalize().toString();
+        }catch (URISyntaxException e) {
+        }
+
+        if(url.contains("?")) {
+            System.out.println(url.indexOf("?"));
+            url = url.substring(0, url.indexOf("?"));
+        }
+        if(url.contains("#")) {
+            url = url.substring(0, url.indexOf("#"));
+        }
+        return url;
+    }
+}
